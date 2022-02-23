9772b5e1effeb181a0cd4017088ff63ac8c78a11
Kauã Sacramento <88597158+KauaBR0@users.noreply.github.com>
2022-02-23 21:47:37 +0000

    Primeira questão feita

---
+++ Exercicios/q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Faça um programa que imprima o seu nome, sua matrícula, eo seu semestre de ingresso no curso. Cada informação em uma linha.
+
+int main()
+{
+  int nome, matricula, semestre;
+  printf("nome: Kauã Figueiredo do Sacramento\n");
+  printf("matricula: 20212160047\n");
+  printf("semestre: 2021.2 \n");
+  
+return 0;
+}
